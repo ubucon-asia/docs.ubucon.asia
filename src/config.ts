9f0a95f9abbec234c68fe99165e7e671cc8ad2dd
Youngbin Han <ybhan@ubuntu.com>
2024-09-07 14:59:41 +0900

    Update site meta

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const Config = {
     siteName: "UbuCon Asia Docs",
-    sideDesc: "",
+    sideDesc: "General and detailed documentation and guidance on UbuCon Asia",
     logo: "",
     favicon: "",
     footer: {
-        copyright: "",
-        info: "",
-        contactEmail: "",
-        srcRepoLabel: "",
-        srcRepoUrl: ""
+        copyright: "Copyright &copy; 2023-Present UbuCon Asia Committee",
+        info: "Ubuntu and Canonical are registered trademarks of Canonical Ltd. Contents of this websites are sources from Ubuntu Discourse",
+        contactEmail: "contact@ubucon.asia",
+        srcRepoLabel: "ubucon-asia/docs.ubucon.asia",
+        srcRepoUrl: "https://github.com/ubucon-asia/docs.ubucon.asia"
     }
 }
